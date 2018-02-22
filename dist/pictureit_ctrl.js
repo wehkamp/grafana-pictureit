2bd2af6cbf7085e26b06fb3763663fca50c30cb5
petervanhespen <pvhespen@wehkamp.nl>
2018-02-22 14:21:38 +0100

    Add support for responsive displays

--- dist/pictureit_ctrl.js
+++ dist/pictureit_ctrl.js
@@ -1,185 +1,228 @@
 'use strict';
 
 System.register(['lodash', 'app/plugins/sdk', './sprintf.js', './angular-sprintf.js', 'app/core/utils/kbn'], function (_export, _context) {
-  "use strict";
+    "use strict";
 
-  var _, MetricsPanelCtrl, kbn, _createClass, panelDefaults, PictureItCtrl;
+    var _, MetricsPanelCtrl, kbn, _createClass, panelDefaults, PictureItCtrl;
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  function _possibleConstructorReturn(self, call) {
-    if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
 
-    return call && (typeof call === "object" || typeof call === "function") ? call : self;
-  }
-
-  function _inherits(subClass, superClass) {
-    if (typeof superClass !== "function" && superClass !== null) {
-      throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
-    }
-
-    subClass.prototype = Object.create(superClass && superClass.prototype, {
-      constructor: {
-        value: subClass,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
-  }
-
-  return {
-    setters: [function (_lodash) {
-      _ = _lodash.default;
-    }, function (_appPluginsSdk) {
-      MetricsPanelCtrl = _appPluginsSdk.MetricsPanelCtrl;
-    }, function (_sprintfJs) {}, function (_angularSprintfJs) {}, function (_appCoreUtilsKbn) {
-      kbn = _appCoreUtilsKbn.default;
-    }],
-    execute: function () {
-      _createClass = function () {
-        function defineProperties(target, props) {
-          for (var i = 0; i < props.length; i++) {
-            var descriptor = props[i];
-            descriptor.enumerable = descriptor.enumerable || false;
-            descriptor.configurable = true;
-            if ("value" in descriptor) descriptor.writable = true;
-            Object.defineProperty(target, descriptor.key, descriptor);
-          }
+    function _possibleConstructorReturn(self, call) {
+        if (!self) {
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
         }
 
-        return function (Constructor, protoProps, staticProps) {
-          if (protoProps) defineProperties(Constructor.prototype, protoProps);
-          if (staticProps) defineProperties(Constructor, staticProps);
-          return Constructor;
-        };
-      }();
+        return call && (typeof call === "object" || typeof call === "function") ? call : self;
+    }
 
-      panelDefaults = {
-        valueMaps: [],
-        seriesList: [],
-        series: [],
-        bgimage: '',
-        sensors: [],
-        height: '400px',
-        width: '100px'
-      };
-
-      _export('PictureItCtrl', PictureItCtrl = function (_MetricsPanelCtrl) {
-        _inherits(PictureItCtrl, _MetricsPanelCtrl);
-
-        function PictureItCtrl($scope, $injector) {
-          _classCallCheck(this, PictureItCtrl);
-
-          var _this = _possibleConstructorReturn(this, (PictureItCtrl.__proto__ || Object.getPrototypeOf(PictureItCtrl)).call(this, $scope, $injector));
-
-          _.defaults(_this.panel, panelDefaults);
-
-          _this.unitFormats = kbn.getUnitFormats();
-
-          _this.events.on('init-edit-mode', _this.onInitEditMode.bind(_this));
-          _this.events.on('panel-initialized', _this.render.bind(_this));
-          _this.events.on('data-received', _this.onDataReceived.bind(_this));
-          _this.events.on('data-snapshot-load', _this.onDataReceived.bind(_this));
-          return _this;
+    function _inherits(subClass, superClass) {
+        if (typeof superClass !== "function" && superClass !== null) {
+            throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
         }
 
-        _createClass(PictureItCtrl, [{
-          key: 'onDataReceived',
-          value: function onDataReceived(dataList) {
-            var dataListLength = dataList.length;
-            this.panel.valueMaps = [];
-            for (var series = 0; series < dataListLength; series++) {
-              this.panel.valueMaps.push({ name: dataList[series].target, value: dataList[series].datapoints[dataList[series].datapoints.length - 1][0] });
+        subClass.prototype = Object.create(superClass && superClass.prototype, {
+            constructor: {
+                value: subClass,
+                enumerable: false,
+                writable: true,
+                configurable: true
             }
+        });
+        if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+    }
 
-            this.render();
-          }
-        }, {
-          key: 'deleteSensor',
-          value: function deleteSensor(index) {
-            this.panel.sensors.splice(index, 1);
-          }
-        }, {
-          key: 'addSensor',
-          value: function addSensor() {
-            if (this.panel.sensors.length == 0) this.panel.sensors.push({ name: 'A-series', xlocation: 200, ylocation: 200, format: 'none', decimals: 'auto', bgcolor: 'rgba(0, 0, 0, 0.58)', color: '#FFFFFF', size: 22, bordercolor: 'rgb(251, 4, 4)', visible: true });else {
-              var lastSensor = this.panel.sensors[this.panel.sensors.length - 1];
-
-              this.panel.sensors.push({ name: lastSensor.name, xlocation: 200, ylocation: 200, format: lastSensor.format, decimals: lastSensor.decimals, bgcolor: lastSensor.bgcolor, color: lastSensor.color, size: lastSensor.size, bordercolor: lastSensor.bordercolor, visible: true });
-            }
-          }
-        }, {
-          key: 'setUnitFormat',
-          value: function setUnitFormat(subItem, index) {
-            this.panel.sensors[index].format = subItem.value;
-          }
-        }, {
-          key: 'onInitEditMode',
-          value: function onInitEditMode() {
-            this.addEditorTab('Options', 'public/plugins/bessler-pictureit-panel/editor.html', 2);
-          }
-        }, {
-          key: 'link',
-          value: function link(scope, elem, attrs, ctrl) {
-            var sensors;
-            var valueMaps;
-
-            var $panelContainer = elem.find('.panel-container');
-
-            function pixelStrToNum(str) {
-              return parseInt(str.substr(0, str.length - 2));
-            }
-
-            function render() {
-              if (!ctrl.panel.sensors) {
-                return;
-              }
-
-              var width = pixelStrToNum($panelContainer.css("width"));
-              var height = pixelStrToNum($panelContainer.css("height"));
-
-              sensors = ctrl.panel.sensors;
-              valueMaps = ctrl.panel.valueMaps;
-
-              var sensorsLength = sensors.length;
-              var valueMapsLength = valueMaps.length;
-
-              for (var sensor = 0; sensor < sensorsLength; sensor++) {
-                sensors[sensor].visible = sensors[sensor].xlocation < width && sensors[sensor].ylocation < height;
-                sensors[sensor].ylocationStr = sensors[sensor].ylocation.toString() + "px";
-                sensors[sensor].xlocationStr = sensors[sensor].xlocation.toString() + "px";
-                sensors[sensor].sizeStr = sensors[sensor].size.toString() + "px";
-                for (var valueMap = 0; valueMap < valueMapsLength; valueMap++) {
-                  if (sensors[sensor].name == valueMaps[valueMap].name) {
-                    sensors[sensor].valueFormatted = kbn.valueFormats[sensors[sensor].format](valueMaps[valueMap].value, sensors[sensor].decimals, null);
-                    break;
-                  }
+    return {
+        setters: [function (_lodash) {
+            _ = _lodash.default;
+        }, function (_appPluginsSdk) {
+            MetricsPanelCtrl = _appPluginsSdk.MetricsPanelCtrl;
+        }, function (_sprintfJs) {}, function (_angularSprintfJs) {}, function (_appCoreUtilsKbn) {
+            kbn = _appCoreUtilsKbn.default;
+        }],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
                 }
-              }
-            }
 
-            this.events.on('render', function () {
-              render();
-              ctrl.renderingCompleted();
-            });
-          }
-        }]);
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
 
-        return PictureItCtrl;
-      }(MetricsPanelCtrl));
+            panelDefaults = {
+                valueMaps: [],
+                seriesList: [],
+                series: [],
+                bgimage: '',
+                sensors: [],
+                height: '400px',
+                width: '100px'
+            };
 
-      _export('PictureItCtrl', PictureItCtrl);
+            _export('PictureItCtrl', PictureItCtrl = function (_MetricsPanelCtrl) {
+                _inherits(PictureItCtrl, _MetricsPanelCtrl);
 
-      PictureItCtrl.templateUrl = 'module.html';
-    }
-  };
+                function PictureItCtrl($scope, $injector) {
+                    _classCallCheck(this, PictureItCtrl);
+
+                    var _this = _possibleConstructorReturn(this, (PictureItCtrl.__proto__ || Object.getPrototypeOf(PictureItCtrl)).call(this, $scope, $injector));
+
+                    _.defaults(_this.panel, panelDefaults);
+
+                    _this.unitFormats = kbn.getUnitFormats();
+                    var bindThis = _this;
+                    _this.events.on('init-edit-mode', _this.onInitEditMode.bind(_this));
+                    _this.events.on('panel-initialized', _this.render.bind(_this));
+                    _this.events.on('data-received', _this.onDataReceived.bind(_this));
+                    _this.events.on('data-snapshot-load', _this.onDataReceived.bind(_this));
+
+                    var img = new Image();
+                    img.addEventListener("load", function () {
+                        bindThis.refImageSize = { w: this.naturalWidth, h: this.naturalHeight };
+                    });
+                    img.src = _this.panel.bgimage;
+                    return _this;
+                }
+
+                _createClass(PictureItCtrl, [{
+                    key: 'onDataReceived',
+                    value: function onDataReceived(dataList) {
+                        var dataListLength = dataList.length;
+                        this.panel.valueMaps = [];
+                        for (var series = 0; series < dataListLength; series++) {
+                            this.panel.valueMaps.push({
+                                name: dataList[series].target,
+                                value: dataList[series].datapoints[dataList[series].datapoints.length - 1][0]
+                            });
+                        }
+
+                        this.render();
+                    }
+                }, {
+                    key: 'deleteSensor',
+                    value: function deleteSensor(index) {
+                        this.panel.sensors.splice(index, 1);
+                    }
+                }, {
+                    key: 'addSensor',
+                    value: function addSensor() {
+                        if (this.panel.sensors.length == 0) this.panel.sensors.push({
+                            name: 'A-series',
+                            xlocation: 200,
+                            ylocation: 200,
+                            format: 'none',
+                            decimals: 'auto',
+                            bgcolor: 'rgba(0, 0, 0, 0.58)',
+                            color: '#FFFFFF',
+                            size: 22,
+                            bordercolor: 'rgb(251, 4, 4)',
+                            visible: true
+                        });else {
+                            var lastSensor = this.panel.sensors[this.panel.sensors.length - 1];
+
+                            this.panel.sensors.push({
+                                name: lastSensor.name,
+                                xlocation: 200,
+                                ylocation: 200,
+                                format: lastSensor.format,
+                                decimals: lastSensor.decimals,
+                                bgcolor: lastSensor.bgcolor,
+                                color: lastSensor.color,
+                                size: lastSensor.size,
+                                bordercolor: lastSensor.bordercolor,
+                                visible: true
+                            });
+                        }
+                    }
+                }, {
+                    key: 'setUnitFormat',
+                    value: function setUnitFormat(subItem, index) {
+                        this.panel.sensors[index].format = subItem.value;
+                    }
+                }, {
+                    key: 'onInitEditMode',
+                    value: function onInitEditMode() {
+                        this.addEditorTab('Options', 'public/plugins/bessler-pictureit-panel/editor.html', 2);
+                    }
+                }, {
+                    key: 'link',
+                    value: function link(scope, elem, attrs, ctrl) {
+                        var sensors;
+                        var valueMaps;
+
+                        var $panelContainer = elem.find('.panel-container');
+
+                        function pixelStrToNum(str) {
+                            return parseInt(str.substr(0, str.length - 2));
+                        }
+
+                        function render() {
+                            if (!ctrl.panel.sensors) {
+                                return;
+                            }
+
+                            var refImage = document.getElementById('imageRef');
+                            if (!refImage) {
+                                return;
+                            }
+                            if (refImage.clientHeight > refImage.clientWidth) {
+                                refImage.style = "width: auto; visibility: hidden; position: absolute";
+                            }
+                            var width = pixelStrToNum($panelContainer.css("width"));
+                            var height = pixelStrToNum($panelContainer.css("height"));
+
+                            sensors = ctrl.panel.sensors;
+                            valueMaps = ctrl.panel.valueMaps;
+                            var sensorsLength = sensors.length;
+                            var valueMapsLength = valueMaps.length;
+                            var imageHeight = refImage.clientHeight;
+                            var imageWidth = refImage.clientWidth;
+                            var originalHeight = ctrl.refImageSize.h;
+                            var originalWidth = ctrl.refImageSize.w;
+
+                            for (var sensor = 0; sensor < sensorsLength; sensor++) {
+                                sensors[sensor].visible = sensors[sensor].xlocation < width && sensors[sensor].ylocation < height;
+                                var calculatedYPos = imageHeight * sensors[sensor].ylocation / originalHeight;
+                                var calculatedXPos = imageWidth * sensors[sensor].xlocation / originalWidth;
+                                sensors[sensor].ylocationStr = calculatedYPos.toString() + "px";
+                                sensors[sensor].xlocationStr = calculatedXPos.toString() + "px";
+                                sensors[sensor].sizeStr = imageWidth * sensors[sensor].size / originalWidth + "px";
+                                for (var valueMap = 0; valueMap < valueMapsLength; valueMap++) {
+                                    if (sensors[sensor].name == valueMaps[valueMap].name) {
+                                        sensors[sensor].valueFormatted = kbn.valueFormats[sensors[sensor].format](valueMaps[valueMap].value, sensors[sensor].decimals, null);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        this.events.on('render', function () {
+                            render();
+                            ctrl.renderingCompleted();
+                        });
+                    }
+                }]);
+
+                return PictureItCtrl;
+            }(MetricsPanelCtrl));
+
+            _export('PictureItCtrl', PictureItCtrl);
+
+            PictureItCtrl.templateUrl = 'module.html';
+        }
+    };
 });
 //# sourceMappingURL=pictureit_ctrl.js.map
--- src/pictureit_ctrl.js
+++ src/pictureit_ctrl.js
@@ -1,107 +1,152 @@
 import _ from 'lodash';
-import {MetricsPanelCtrl} from 'app/plugins/sdk';
+import { MetricsPanelCtrl } from 'app/plugins/sdk';
 import './sprintf.js';
 import './angular-sprintf.js';
 import kbn from 'app/core/utils/kbn';
 
 const panelDefaults = {
-	valueMaps: [],
-	seriesList: [],
-	series: [],
-	bgimage: '',
-	sensors: [],
-	height: '400px',
-	width: '100px'
+    valueMaps: [],
+    seriesList: [],
+    series: [],
+    bgimage: '',
+    sensors: [],
+    height: '400px',
+    width: '100px'
 };
 
-export class PictureItCtrl extends MetricsPanelCtrl  {
-  
-  
-  constructor($scope, $injector) {
-    super($scope, $injector);
-    _.defaults(this.panel, panelDefaults);
+export class PictureItCtrl extends MetricsPanelCtrl {
 
-    this.unitFormats = kbn.getUnitFormats();
+    constructor($scope, $injector) {
+        super($scope, $injector);
+        _.defaults(this.panel, panelDefaults);
 
-    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
-    this.events.on('panel-initialized', this.render.bind(this));
-    this.events.on('data-received', this.onDataReceived.bind(this));
-    this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
-  }
+        this.unitFormats = kbn.getUnitFormats();
+        var bindThis = this;
+        this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+        this.events.on('panel-initialized', this.render.bind(this));
+        this.events.on('data-received', this.onDataReceived.bind(this));
+        this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
 
-  onDataReceived(dataList) {	
-	var dataListLength = dataList.length;
-	this.panel.valueMaps=[];
-	for (var series=0;series<dataListLength;series++) {		
-		this.panel.valueMaps.push({name:dataList[series].target,value:dataList[series].datapoints[dataList[series].datapoints.length-1][0]});
-	}
-	
-    this.render();
-  }
-  
-  deleteSensor(index) {
-	this.panel.sensors.splice(index,1);
-  }
-  
-  addSensor() {
-	if (this.panel.sensors.length==0)
-		this.panel.sensors.push({name: 'A-series', xlocation: 200,ylocation: 200,format: 'none', decimals: 'auto',bgcolor:'rgba(0, 0, 0, 0.58)',color:'#FFFFFF',size:22, bordercolor:'rgb(251, 4, 4)',visible:true});
-	else {
-		var lastSensor=this.panel.sensors[this.panel.sensors.length-1];
-	
-		this.panel.sensors.push({name: lastSensor.name, xlocation: 200,ylocation: 200,format: lastSensor.format, decimals: lastSensor.decimals,bgcolor:lastSensor.bgcolor,color:lastSensor.color,size:lastSensor.size, bordercolor:lastSensor.bordercolor,visible:true});
-	}
-  }
-  
-  setUnitFormat(subItem, index) {
-  	this.panel.sensors[index].format = subItem.value;
-  }
-
-  onInitEditMode() {
-    this.addEditorTab('Options', 'public/plugins/bessler-pictureit-panel/editor.html', 2);
-  }
-	
-  link(scope, elem, attrs, ctrl) {
-    var sensors;
-    var valueMaps;
-
-    const $panelContainer = elem.find('.panel-container');
-
-    function pixelStrToNum(str) {
-        return parseInt(str.substr(0,str.length-2));
+        var img = new Image();
+        img.addEventListener("load", function () {
+            bindThis.refImageSize= {w: this.naturalWidth, h:this.naturalHeight}
+        });
+        img.src = this.panel.bgimage;
     }
-	
-    function render() {
-        if (!ctrl.panel.sensors) { return; }
-	
-        var width = pixelStrToNum($panelContainer.css("width"));
-        var height = pixelStrToNum($panelContainer.css("height"));
-	  
-        sensors = ctrl.panel.sensors;	  
-	valueMaps = ctrl.panel.valueMaps;
-	  
-	var sensorsLength = sensors.length;
-	var valueMapsLength = valueMaps.length;
-	  
-	for (var sensor=0;sensor<sensorsLength;sensor++) {
-            sensors[sensor].visible = sensors[sensor].xlocation<width && sensors[sensor].ylocation<height;
-            sensors[sensor].ylocationStr=sensors[sensor].ylocation.toString()+"px";
-            sensors[sensor].xlocationStr=sensors[sensor].xlocation.toString()+"px";
-            sensors[sensor].sizeStr=sensors[sensor].size.toString()+"px";
-            for (var valueMap=0;valueMap<valueMapsLength;valueMap++) {	
-                if (sensors[sensor].name==valueMaps[valueMap].name) {
-                    sensors[sensor].valueFormatted = kbn.valueFormats[sensors[sensor].format](valueMaps[valueMap].value,sensors[sensor].decimals, null);
-		break;
-		}
+
+    onDataReceived(dataList) {
+        var dataListLength = dataList.length;
+        this.panel.valueMaps = [];
+        for (var series = 0; series < dataListLength; series++) {
+            this.panel.valueMaps.push({
+                name: dataList[series].target,
+                value: dataList[series].datapoints[dataList[series].datapoints.length - 1][0]
+            });
+        }
+
+        this.render();
+    }
+
+    deleteSensor(index) {
+        this.panel.sensors.splice(index, 1);
+    }
+
+    addSensor() {
+        if (this.panel.sensors.length == 0)
+            this.panel.sensors.push({
+                name: 'A-series',
+                xlocation: 200,
+                ylocation: 200,
+                format: 'none',
+                decimals: 'auto',
+                bgcolor: 'rgba(0, 0, 0, 0.58)',
+                color: '#FFFFFF',
+                size: 22,
+                bordercolor: 'rgb(251, 4, 4)',
+                visible: true
+            });
+        else {
+            var lastSensor = this.panel.sensors[this.panel.sensors.length - 1];
+
+            this.panel.sensors.push({
+                name: lastSensor.name,
+                xlocation: 200,
+                ylocation: 200,
+                format: lastSensor.format,
+                decimals: lastSensor.decimals,
+                bgcolor: lastSensor.bgcolor,
+                color: lastSensor.color,
+                size: lastSensor.size,
+                bordercolor: lastSensor.bordercolor,
+                visible: true
+            });
+        }
+    }
+
+    setUnitFormat(subItem, index) {
+        this.panel.sensors[index].format = subItem.value;
+    }
+
+    onInitEditMode() {
+        this.addEditorTab('Options', 'public/plugins/bessler-pictureit-panel/editor.html', 2);
+    }
+
+    link(scope, elem, attrs, ctrl) {
+        var sensors;
+        var valueMaps;
+
+        const $panelContainer = elem.find('.panel-container');
+
+        function pixelStrToNum(str) {
+            return parseInt(str.substr(0, str.length - 2));
+        }
+
+        function render() {
+            if (!ctrl.panel.sensors) {
+                return;
             }
-	}
-    }
 
-    this.events.on('render', function() {
-      render();
-      ctrl.renderingCompleted();
-    });
-  }
+            var refImage = document.getElementById('imageRef');
+            if(!refImage){
+                return;
+            }
+                if(refImage.clientHeight > refImage.clientWidth){
+                    refImage.style = "width: auto; visibility: hidden; position: absolute";
+                }
+            var width = pixelStrToNum($panelContainer.css("width"));
+            var height = pixelStrToNum($panelContainer.css("height"));
+
+            sensors = ctrl.panel.sensors;
+            valueMaps = ctrl.panel.valueMaps;
+            var sensorsLength = sensors.length;
+            var valueMapsLength = valueMaps.length;
+            var imageHeight = refImage.clientHeight;
+            var imageWidth = refImage.clientWidth;
+            var originalHeight = ctrl.refImageSize.h;
+            var originalWidth = ctrl.refImageSize.w;
+
+            for (var sensor = 0; sensor < sensorsLength; sensor++) {
+                sensors[sensor].visible = sensors[sensor].xlocation < width && sensors[sensor].ylocation < height;
+                var calculatedYPos = imageHeight * sensors[sensor].ylocation / originalHeight;
+                var calculatedXPos = imageWidth * sensors[sensor].xlocation / originalWidth;
+                sensors[sensor].ylocationStr = (calculatedYPos).toString() + "px";
+                sensors[sensor].xlocationStr = (calculatedXPos).toString() + "px";
+                sensors[sensor].sizeStr = imageWidth * sensors[sensor].size / originalWidth + "px";
+                for (var valueMap = 0; valueMap < valueMapsLength; valueMap++) {
+                    if (sensors[sensor].name == valueMaps[valueMap].name) {
+                        sensors[sensor].valueFormatted = kbn.valueFormats[sensors[sensor].format](valueMaps[valueMap].value, sensors[sensor].decimals, null);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        this.events.on('render', function () {
+            render();
+            ctrl.renderingCompleted();
+        });
+    }
 }
 
 PictureItCtrl.templateUrl = 'module.html';
